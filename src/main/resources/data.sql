840abc431814862c039269af92c2b9317d613d72
taunnol <rooimeecarp@gmail.com>
2025-12-24 03:16:54 +0300

    add default data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO items (title, description, img_path, price) VALUES
+('Картофель', 'Крупный картофель 1 кг', 'images/potato.jpg', 67),
+('Томаты', 'Томаты черри на ветке 250 г', '/images/tomato.jpg', 250),
+('Яблоки', 'Яблоки зеленые 700 г', '/images/apple.jpg', 100),
+('Вода питьевая', 'Вода питьевая негазированная 5 л', '/images/water.jpg', 150),
+('Сахар', 'Сахар песок 1 кг', '/images/sugar.jpg', 72),
+('Огурцы', 'Огурцы свежие 500 г', '/images/cucumber.jpg', 200),
+('Дикий огурец', 'Дикий огурец 1 шт', '/images/wild-cucumber.jpg', 10000);
