e8fe1a3223d7b21c4083d17e838111c1449c936d
taunnol <rooimeecarp@gmail.com>
2026-01-16 06:53:02 +0300

    cleanup sql

--- market-app/src/main/resources/data.sql
+++ market-app/src/main/resources/data.sql
@@ -1,7 +1,23 @@
-INSERT INTO users(username, password_hash, role) VALUES
-('user', '$2b$10$TEiiV3aHCKmvv1AsW55Q8uF0v3Ql.4DA5ZIzs4njSQf4abwp8urj2', 'USER'), -- password
-('user2', '$2b$10$4fnYiyd4HPpOLIef3VJJAOem1ucc3N/MiPOvkKcFBh.bM2sOS60aa', 'USER'), -- password2
-('admin', '$2b$10$eGP2XZ86cHXlNxkcUdFyV.TtZ2qC6W8oyWiZJvSeBY0DlHLzyDuLO', 'ADMIN'); -- admin
+INSERT INTO users(username, password_hash, role)
+SELECT
+    'user',
+    '$2b$10$TEiiV3aHCKmvv1AsW55Q8uF0v3Ql.4DA5ZIzs4njSQf4abwp8urj2',
+    'USER'
+WHERE NOT EXISTS (SELECT 1 FROM users WHERE username = 'user');  -- password
+
+INSERT INTO users(username, password_hash, role)
+SELECT
+    'user2',
+    '$2b$10$4fnYiyd4HPpOLIef3VJJAOem1ucc3N/MiPOvkKcFBh.bM2sOS60aa',  -- password2
+    'USER'
+WHERE NOT EXISTS (SELECT 1 FROM users WHERE username = 'user2');
+
+INSERT INTO users(username, password_hash, role)
+SELECT
+    'admin',
+    '$2b$10$eGP2XZ86cHXlNxkcUdFyV.TtZ2qC6W8oyWiZJvSeBY0DlHLzyDuLO',  -- admin
+    'ADMIN'
+WHERE NOT EXISTS (SELECT 1 FROM users WHERE username = 'admin');
 
 INSERT INTO items (title, description, img_path, price) VALUES
 ('Картофель', 'Крупный картофель 1 кг', 'images/potato.jpg', 67),
